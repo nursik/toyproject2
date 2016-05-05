3144bd4c2f64a87c3b0553eafe57cdc0a86d254a
nursik <nursultanzarlyk@gmail.com>
2016-05-05 10:18:33 +0600

    Trying to start

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+function GameFabric() {
+    var gameState;
+    var score;
+    function up() {
+        
+    }
+    function down() {
+        
+    }
+    function left() {
+        
+    }
+    function right() {
+        
+    }
+}
+var Game = new GameFabric();
+window.onload = function() {
+    
+}
